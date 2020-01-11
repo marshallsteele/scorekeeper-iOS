5c786ba0c0eda7ab406159ca13dc48bbea7016f0
Marshall Steele <marshallsteele123@hotmail.com>
2020-01-11 16:36:27 -0500

    Created XCode Project

---
+++ scorekeeper-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  scorekeeper-iOS
+//
+//  Created by Marshall Steele on 2020-01-11.
+//  Copyright © 2020 tyshall. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ scorekeeper-iOS/LeftSideViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LeftSideViewController.swift
+//  scorekeeper-iOS
+//
+//  Created by Marshall Steele on 2020-01-11.
+//  Copyright © 2020 tyshall. All rights reserved.
+//
+
+import UIKit
+
+class LeftSideViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
